8f92b9240323d226f30439a7d9ea93c72bea280f
CPaulo18 <paulo.ramon.caceres@gmail.com>
2023-07-01 23:18:42 -0300

    actualizar detail

--- src/app/components/education/update-education/update-education.component.ts
+++ src/app/components/education/update-education/update-education.component.ts
@@ -6,35 +6,39 @@ import { SEducationService } from 'src/app/service/s-education.service';
 @Component({
   selector: 'app-update-education',
   templateUrl: './update-education.component.html',
-  styleUrls: ['./update-education.component.css']
+  styleUrls: ['./update-education.component.css'],
 })
-export class UpdateEducationComponent implements OnInit{
-  education: Education = null
-  constructor(private sEducation: SEducationService, private router: Router,
-    private activedRouter: ActivatedRoute){}
+export class UpdateEducationComponent implements OnInit {
+  education: Education = null;
+  constructor(
+    private sEducation: SEducationService,
+    private router: Router,
+    private activedRouter: ActivatedRoute
+  ) {}
 
-
-    ngOnInit(){
-     
-      const id = this.activedRouter.snapshot.params["id"];
-      this.sEducation.detail(id).subscribe(data=>{
-        this.education = data
-      }, err=>{
-        alert("error to update the experice")
-        this.router.navigate([""])
-      })
-    }
-
-
-  onUpdate(): void{
-    const id = this.activedRouter.snapshot.params["id"];
-    this.sEducation.update(id, this.education).subscribe(
-      data=>{
-        this.router.navigate([""]);
-      },err=>{
-        alert("error to update experience")
-        this.router.navigate([""]);
+  ngOnInit() {
+    const id = this.activedRouter.snapshot.params['id'];
+    this.sEducation.detail(id).subscribe(
+      (data) => {
+        this.education = data;
+      },
+      (err) => {
+        alert('error to update the experice');
+        this.router.navigate(['']);
       }
-    )
+    );
+  }
+
+  onUpdate(): void {
+    const id = this.activedRouter.snapshot.params['id'];
+    this.sEducation.update(id, this.education).subscribe(
+      (data) => {
+        this.router.navigate(['']);
+      },
+      (err) => {
+        alert('error to update experience');
+        this.router.navigate(['']);
+      }
+    );
   }
 }
--- src/app/components/experience/update-experience/update-experience.component.ts
+++ src/app/components/experience/update-experience/update-experience.component.ts
@@ -6,36 +6,39 @@ import { SExperienceService } from 'src/app/service/s-experience.service';
 @Component({
   selector: 'app-update-experience',
   templateUrl: './update-experience.component.html',
-  styleUrls: ['./update-experience.component.css']
+  styleUrls: ['./update-experience.component.css'],
 })
-export class UpdateExperienceComponent implements OnInit{
-  expLab: Experience = null
-  constructor(private sExperience: SExperienceService, private router: Router,
-    private activedRouter: ActivatedRoute){}
+export class UpdateExperienceComponent implements OnInit {
+  expLab: Experience = null;
+  constructor(
+    private sExperience: SExperienceService,
+    private router: Router,
+    private activedRouter: ActivatedRoute
+  ) {}
 
-
-    ngOnInit(){
-     
-      const id = this.activedRouter.snapshot.params["id"];
-      this.sExperience.detail(id).subscribe(data=>{
-        this.expLab = data
-      }, err=>{
-        alert("error to update the experice")
-        this.router.navigate([""])
-      })
-    }
-
-
-  onUpdate(): void{
-    const id = this.activedRouter.snapshot.params["id"];
-    this.sExperience.update(id, this.expLab).subscribe(
-      data=>{
-        this.router.navigate([""]);
-      },err=>{
-        alert("error to update experience")
-        this.router.navigate([""]);
+  ngOnInit(): void {
+    const id = this.activedRouter.snapshot.params['id'];
+    this.sExperience.detail(id).subscribe(
+      (data) => {
+        this.expLab = data;
+      },
+      (err) => {
+        alert('error to update the experice');
+        this.router.navigate(['']);
       }
-    )
+    );
   }
 
+  onUpdate(): void {
+    const id = this.activedRouter.snapshot.params['id'];
+    this.sExperience.update(id, this.expLab).subscribe(
+      (data) => {
+        this.router.navigate(['']);
+      },
+      (err) => {
+        alert('error to update experience');
+        this.router.navigate(['']);
+      }
+    );
+  }
 }
